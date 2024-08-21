6aec98fc8e58a1541979f02fd28a6f9d0d59b077
unknown <abhishekrock796@gmail.com>
2024-08-21 11:43:02 +0530

    add overlay of movie description when hover over movie card

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -3,13 +3,19 @@ import { IMAGE_URL_PREFIX_500 } from "../utils/constants";
 
 const MovieCard = ({ movie }) => {
   return (
-        <div className="px-2">
+        <div className="px-2 relative group">
         <div className="w-48">
             <img
             alt="image-poster"
             src={IMAGE_URL_PREFIX_500 + movie.poster_path}
             />
         </div>
+        <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <div className="text-white p-4">
+          <h3 className="text-lg font-bold">{movie.title}</h3>
+          <p className="text-sm">{movie.overview}</p>
+        </div>
+      </div>
         </div>
   );
 };
